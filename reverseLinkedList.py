2fb0ba8b96d90d852208427a6d770bc9b71c615b
Alex Martinez <alexmartinez0071234@gmail.com>
2020-10-23 12:22:25 -0400

    reversed a linked list

---
+++ reverseLinkedList.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        # setup up a prev variable to know
+        prev = None
+        # while there is a head
+        while head:
+            # we need to store a the head as a temp variable
+            temp = head
+            # then traverse the linked list
+            head = head.next
+            # now the pointer of the temp( head ) points to the previous node
+            temp.next = prev
+            # then the previous node changes to the temp which is the next head
+            prev = temp
+        return prev
+        
