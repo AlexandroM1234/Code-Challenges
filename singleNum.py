94196846a1960ebe6e5eb38eefd0b8e604358398
Alex Martinez <alexmartinez0071234@gmail.com>
2021-01-12 12:35:33 -0500

    single number leetcode

---
+++ singleNum.py
@@ -0,0 +1,43 @@
+"""
+Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+Follow up: Could you implement a solution with a linear runtime complexity and without using extra memory?
+
+ 
+
+Example 1:
+
+Input: nums = [2,2,1]
+Output: 1
+
+Example 2:
+Input: nums = [4,1,2,1,2]
+Output: 4
+
+Example 3:
+Input: nums = [1]
+Output: 1
+Constraints:
+
+1 <= nums.length <= 3 * 104
+-3 * 104 <= nums[i] <= 3 * 104
+Each element in the array appears twice except for one element which appears only once.
+"""
+
+
+class Solution:
+    def singleNumber(self, nums: [int]) -> int:
+        # if the length of nums is 1 there is only one item in the list so you can just return it
+        # otherwise set up a hashtable and the keys will be the numbers in the list and the value will be the number of times they show up in the list
+        # once the hashtable is set loop through it and then return the number whose value is 1
+        if len(nums) == 1:
+            return nums[0]
+        count = {}
+        for num in nums:
+            if num not in count:
+                count[num] = 1
+            else:
+                count[num] += 1
+        for num in count:
+            if count[num] == 1:
+                return num
