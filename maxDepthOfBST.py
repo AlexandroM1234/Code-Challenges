581b4364848feb7d910b6e58f1d7b6296a4c8647
Alex Martinez <alexmartinez0071234@gmail.com>
2022-02-14 15:19:52 -0500

    Max depth of bst itterative solution

--- maxDepthOfBST.py
+++ maxDepthOfBST.py
@@ -30,9 +30,41 @@ class TreeNode:
 
 
 class Solution:
+    # Recurisve
     def maxDepth(self, root: TreeNode) -> int:
         if not root:
             return 0
         left = self.maxDepth(root.left)
         right = self.maxDepth(root.right)
         return max(left, right) + 1
+
+    # Itterative Solution
+    def maxDepth2(self, root: Optional[TreeNode]) -> int:
+
+        # stack for dfs because we are looking for the max depth
+        s = []
+        # have an output variable to keep track of the largest value
+        output = 0
+        # a depth variable to keep track of the depth as we go
+        depth = 1
+        # start the stack with the root and the depth we start at which is 1
+        s.append([root, depth])
+
+        # if there is no root return output which to start is 0
+        if not root:
+            return output
+
+        # while there are nodes in the stack
+        while len(s):
+            # pop the node and the depth
+            node, depth = s.pop()
+            # compare the current depth with the max depth
+            output = max(output, depth)
+            # continue traversing if there are left and right nodes
+            if node.left:
+                s.append([node.left, depth + 1])
+
+            if node.right:
+                s.append([node.right, depth + 1])
+        # then return the max depth
+        return output
