3119a6fb486f20900cd9db287680d0c3799507ec
Alex Martinez <alexmartinez0071234@gmail.com>
2022-04-23 12:55:56 -0400

    Two sum v2 with enumerate

--- TwoSum.py
+++ TwoSum.py
@@ -9,3 +9,12 @@ class Solution:
                 return [check[num], i]
             else:
                 check[comp] = i
+
+    def twoSumV2(self, nums: List[int], target: int) -> List[int]:
+        check = {}
+        for i, num in enumerate(nums):
+            comp = target - num
+            if num in check:
+                return [check[num], i]
+            else:
+                check[comp] = i
